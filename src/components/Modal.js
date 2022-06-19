12578034e7e8de8f30d8d4e7db38fa90aa12903a
oyindamola <oyindamoladawodu22@gmail.com>
2022-06-19 22:04:03 +0100

    tooltip added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,48 +1,70 @@
-import React from 'react';
-import { Button, Modal, ModalBody, ModalFooter, Card, CardTitle, CardText } from 'reactstrap';
+import React from "react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  Card,
+  CardTitle,
+  CardText,
+} from "reactstrap";
 
+const ModalExample = ({
+  // className,
+  modal,
+  id,
+  title,
+  body,
+  handleToggle,
+  handleChange,
+  handleUpdate,
+}) => {
+  const handleClick = () => {
+    handleUpdate(id);
+  };
 
-const ModalExample = ({ className, modal,id, title, body, handleToggle, handleChange, handleUpdate }) => {
-
-    const handleClick = () => {
-        handleUpdate(id);
-      }
-
-    return (
-      
-          <Modal isOpen={modal} toggle={handleToggle} className={className} centered>
-            <ModalBody>
-                <div className="NoteCard" >
-                <Card className="stickyCard">
-                    <div>
-                            <CardTitle tag="h5">
-                                  <input
-                                    value={title}
-                                    type="text"
-                                    name="title"
-                                    onChange={handleChange}
-                                  />
-                              </CardTitle>
-                              <CardText tag="p">
-                                  <input
-                                    value={body}
-                                    type="text"
-                                    name="body"
-                                    onChange={handleChange} 
-                                  />
-                              </CardText>
-                    </div>
-                    </Card>
-                    <div className="IconContainer">
-                        <ModalFooter className="modalFooter">
-                            <Button color="secondary" onClick={handleClick}>Submit</Button>{' '}
-                            <Button color="secondary" onClick={handleToggle}>Cancel</Button>
-                        </ModalFooter>
-                    </div>
-                </div>
-            </ModalBody>   
-      </Modal>
+  return (
+    <Modal isOpen={modal} toggle={handleToggle} className="modal" centered>
+      <ModalBody>
+        <div className="NoteCard">
+          <Card className="stickyCard">
+            <div>
+              <CardTitle tag="h5">Edit Note</CardTitle>
+              <CardText tag="p">
+                <input
+                  value={title}
+                  type="text"
+                  name="title"
+                   className="inputs"
+                  onChange={handleChange}
+                />
+              </CardText>
+              <CardText tag="p">
+                <textarea
+                  value={body}
+                  type="text"
+                  name="body"
+                  className="input2"
+                  onChange={handleChange}
+                  rows="3">
+                </textarea>
+              </CardText>
+            </div>
+          </Card>
+          <div className="IconContainer">
+            <ModalFooter className="modalFooter">
+              <Button color="primary" onClick={handleClick}>
+                Submit
+              </Button>{" "}
+              <Button color="secondary" onClick={handleToggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </div>
+        </div>
+      </ModalBody>
+    </Modal>
   );
-}
+};
 
 export default ModalExample;
--- src/components/StickyNote.js
+++ src/components/StickyNote.js
@@ -22,22 +22,11 @@ const StickyNote = (props) => {
   const handleClick = () => {
     editNote(id);
   }
-
-  // const handleClick = (e) => {
-  //   console.log(e.target.parentNode.classList);
-  //   if (e.target.parentNode.classList.contains("buttonContainer") || e.target.parentNode.classList.contains("ColorButton")  || e.target.parentNode.classList.contains("IconContainer")) {
-  //     setClick(false);
-  //     console.log(click)
-  //   } else {
-  //     console.log(click)
-  //     handleToggle();
-  //   }
-  // }
  
   return (
       <StickyCard className="NoteCard" >
-      <Card className="stickyCard" onClick={handleClick}>
-            <div>
+      <Card className="stickyCard" onClick={handleClick} title="Click to Edit Note..">
+            <div title="Click to Edit Note..">
               <CardTitle tag="h5">{title}</CardTitle>
               <CardText hag="p">{body}</CardText>
             </div>
